1b1df7b66255f63778300e915393eaf69e9def88
AlexStepanchuk <666stepashka666@gmail.com>
2020-09-29 22:55:22 +0300

    1st changes

--- DRAGNDROP/script.js
+++ DRAGNDROP/script.js
@@ -1,1 +1,14 @@
 "use strict";
+
+function addObj() {
+  var areaForAction = document.getElementById("DRnDRarea");
+  var quantityOfObj = document.getElementById("qOfObj").value;
+  for (var i = 0; i < quantityOfObj; i++) {
+    var objDND = document.createElement("img");
+    objDND.src = "pic/win.png";
+    objDND.setAttribute("class", "DRAGme");
+    objDND.style.left = Math.floor(Math.random() * 300) + "px";
+    objDND.style.top = Math.floor(Math.random() * 300) + "px";
+    areaForAction.appendChild(objDND);
+  }
+}

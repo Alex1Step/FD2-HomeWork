5037e2488ea4df1510e0a2ef3e582fac1ca0f11e
AlexStepanchuk <666stepashka666@gmail.com>
2020-10-01 22:40:30 +0300

    v1.0

--- DRAGNDROP/script.js
+++ DRAGNDROP/script.js
@@ -1,44 +1,71 @@
 "use strict";
 
-function addObj() {
-  var areaForAction = document.getElementById("DRnDRarea");
-  var quantityOfObj = document.getElementById("qOfObj").value;
-  for (var i = 0; i < quantityOfObj; i++) {
-    var objDND = document.createElement("img");
-    objDND.src = "pic/win.png";
-    objDND.setAttribute("class", "DRAGme");
-    objDND.style.left = Math.floor(Math.random() * 300) + "px";
-    objDND.style.top = Math.floor(Math.random() * 300) + "px";
-    areaForAction.appendChild(objDND);
-    objDND.onmousedown = mdF;
-    objDND.onmouseup = muF;
-    objDND.onmousemove = mmF;
+window.addEventListener("DOMContentLoaded", documentReady, false);
+
+function documentReady() {
+  var dragArea = document.getElementById("DRnDRarea");
+  var allImg = dragArea.getElementsByTagName("img");
+  for (var i = 0; i < allImg.length; i++) {
+    // для каждого элемента: даём position:absolute и позиционируем в координаты
+    var posTop = allImg[i].offsetTop;
+    var posLeft = allImg[i].offsetLeft;
+    console.log(posTop + "   " + posLeft);
+    allImg[i].style.top = posTop + "px";
+    allImg[i].style.left = posLeft + "px";
+    //подписываемся на событие нажатия левой клавиши мыши
+    allImg[i].addEventListener("mousedown", mdF, false);
+  }
+  for (var i = 0; i < allImg.length; i++) {
+    allImg[i].style.position = "absolute";
   }
 }
 
-var mousePosX = 0;
-var mousePosY = 0;
-
+//обработка нажатия левой клавиши мыши
 function mdF(EO) {
   var EO = EO || window.event;
   EO.preventDefault();
+  EO.target.style.cursor = "move";
   EO.target.style.position = "absolute";
-  EO.target.style.zIndex = 1000;
-  //   console.log(EO);
-  mousePosX = EO.pageX;
-  mousePosY = EO.pageY;
-}
+  EO.target.style.zIndex = 10;
+  var relPosX = EO.clientX - EO.target.getBoundingClientRect().left;
+  var relPosY = EO.clientY - EO.target.getBoundingClientRect().top;
 
-function muF(EO) {
-  var EO = EO || window.event;
-  EO.preventDefault();
-  EO.target.style.zIndex = 0;
-  //   console.log(EO.target);
-}
+  //прослушиваем события движения мыши и отпускания клавиши мыши
+  EO.target.addEventListener("mousemove", mmF, false);
+  EO.target.addEventListener("mouseup", muF, false);
 
-function mmF(EO) {
-  var EO = EO || window.event;
-  EO.preventDefault();
-  EO.target.style.left = EO.target.pageX - mousePosX + "px";
-  EO.target.style.top = EO.target.pageY - mousePosY + "px";
+  //обработка отпускания левой клавиши мыши
+  function muF(EO) {
+    var EO = EO || window.event;
+    EO.preventDefault();
+    EO.target.style.position = "absolute";
+    EO.target.style.zIndex = 0;
+    EO.target.removeEventListener("mousemove", mmF);
+    EO.target.removeEventListener("mouseup", muF);
+  }
+
+  //обработка движения мыши с зажатой левой клавишей
+  function mmF(EO) {
+    //определяем левая ли клавиша была нажата
+    if (EO.which === 1) {
+      var EO = EO || window.event;
+      EO.preventDefault();
+      var coordX = EO.pageX - relPosX;
+      var coordY = EO.pageY - relPosY;
+      var drndrArea = document.getElementById("DRnDRarea");
+      var minLimitX = drndrArea.getBoundingClientRect().left / 2;
+      var minLimitY = drndrArea.getBoundingClientRect().top / 2;
+      var maxLimitX = drndrArea.getBoundingClientRect().right - minLimitX;
+      var maxLimitY = drndrArea.getBoundingClientRect().bottom - minLimitY;
+      if (
+        coordX > minLimitX &&
+        coordX < maxLimitX - EO.target.offsetWidth &&
+        coordY > minLimitY &&
+        coordY < maxLimitY - EO.target.offsetHeight
+      ) {
+        EO.target.style.left = coordX + "px";
+        EO.target.style.top = coordY + "px";
+      } else muF(EO);
+    } else return false;
+  }
 }
